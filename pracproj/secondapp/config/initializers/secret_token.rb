244103011706870726b5cc1c17847a4d21a78952
Hariharan <nhary91@gmail.com>
2014-02-06 16:03:53 +0530

    second app

---
+++ pracproj/secondapp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Secondapp::Application.config.secret_key_base = '98d42d5d9f9237eb31c135896b43a53008017920f3ffd275747093dd605b0cb6db2aabe1f11a91a410c9ee1a6c725dffbfa426e6ab518c8bf3fbabcd3177a71d'
